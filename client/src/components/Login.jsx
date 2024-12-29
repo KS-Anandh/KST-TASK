ef9359ed5d078fdd8a4d2cba8e3627d0814bc759
KS-Anandh <167880483+KS-Anandh@users.noreply.github.com>
2024-12-29 18:10:34 +0530

    Update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
       return;
     }
     try {
-      const res = await axios.post("http://localhost:9600/users/login", {
+      const res = await axios.post("https://kst-project.vercel.app/users/login", {
         userMail,
         userPassword,
       });
